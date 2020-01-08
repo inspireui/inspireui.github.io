c53b77c43c0a7d98ed37de11e156d577e9b5e27e
Minh <minh@inspireui.com>
2020-01-08 08:15:24 +0700

    update fluxstore

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-a6683a2048df7dcd40da.js"
+    "url": "webpack-runtime-30cc2240d9f05c709205.js"
   },
   {
     "url": "app-78dd31335c98389e3352.js"
@@ -36,7 +36,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "e5834051602d2f89302a15abb1b0e220"
+    "revision": "97192f2b12f626237677e8d6f408772a"
   },
   {
     "url": "styles.65aa3ebb07d422711f9d.css"
