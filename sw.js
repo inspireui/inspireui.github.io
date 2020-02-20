9f89976ff27747b10f1e9fdb2c41e770fde9c80c
minh pham <minhcas@gmail.com>
2020-02-20 19:43:13 +0700

    up

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-7c5711338ea874360ab0.js"
+    "url": "webpack-runtime-65a81547db92c65e2116.js"
   },
   {
     "url": "app-77f8851d045ba0563ce3.js"
@@ -36,7 +36,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "749dcf29a6b00bb72c3bf42d8a15e9e3"
+    "revision": "f6f5efb5a55c725e1143b939bc74e22c"
   },
   {
     "url": "styles.65aa3ebb07d422711f9d.css"
